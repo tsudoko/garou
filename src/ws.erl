1a880a2c32a78750f9454cddd10418acf6f5eecc
flan <flan@flande.re>
2018-07-03 15:35:10 +0200

    Add big TODO

--- src/ws.erl
+++ src/ws.erl
@@ -1,4 +1,29 @@
 -define(WS_GUID, "258EAFA5-E914-47DA-95CA-C5AB0DC85B11").
 
+% pedantic TODO (server only):
+%  - description (RFC 6455 section [, ([listnumber[#itemnumber|details)]) (how to handle)
+%  - handshake URI must be a valid HTTP URI without '#' (3) (7.1.7 fail)
+%  - only one CONNECTING connection from a given IP at a time (4.1, 1#2) (???)
+%  - handle SNI somehow? (4.1, 1#5 and 4.2.2, #1) (???)
+%    - this seems annoying enough to justify not supporting TLS altogether
+%      (i.e. offload to some reverse proxy)
+%  - only GETs, only HTTP/1.1 or higher (4.1, 2#2 and 4.2.1, #1) (400)
+%  - non-80 (for non-ssl connections)/443 (for ssl connections) ports must be
+%    specified explicitly in the Host header (4.1, 2#4) (???)
+%  - Upgrade must include "websocket" (4.1, 2#5 and 4.2.1, #3) (400)
+%  - Connection must include "Upgrade" (4.1, 2#6 and 4.2.1, #4) (400)
+%  - Sec-Websocket-Key must be a base64-encoded 16-byte value (4.1, 2#7 and 4.2.1, #5) (400)
+%  - Sec-Websocket-Version must be 13 (4.1, 2#9 and 4.2.1, #6) (400)
+%  - the 4 headers mentioned above are required in the handshake
+%  - response must be 101, Upgrade=="websocket", Connection=="upgrade", Sec-Websocket-Accept blah (4.2.2 #5)
+%  - include sec-websocket-version in 400 responses
+%  - storing the origin somewhere might be desirable for later validation (4.2.2 #4)
+%    (wouldn't it be better to let a reverse proxy handle this?)
+%  - subprotocol support
+
+%  - all frames from the client must be masked (Close with 1002)
+%  - all frames from the server must not be masked
+%  - unknown opcode -> 7.1.7 fail
+
 sec_websocket_accept(Key) ->
 	base64:encode(crypto:hash(sha, [Key, WS_GUID])).
