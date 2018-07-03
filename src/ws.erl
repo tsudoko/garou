46353c590a4442645d899d5a848547799f34a429
flan <flan@flande.re>
2018-07-03 22:56:07 +0200

    Fix type of MaskKey
    Default is integer

--- src/ws.erl
+++ src/ws.erl
@@ -47,7 +47,7 @@ unmask(Key, Data, R) when byte_size(Key) > byte_size(Data) ->
 
 %decode_frame(_) -> % ???? is this needed?
 %	{more, undefined};
-decode_frame(<<Fin:1, Reserved:3, Op:4, 1:1, 127:7, Len:64, MaskKey:32, Rest/bytes>>) ->
+decode_frame(<<Fin:1, Reserved:3, Op:4, 1:1, 127:7, Len:64, MaskKey:4/bytes, Rest/bytes>>) ->
 	decode_frame(<<Fin:1, Reserved:3, Op:4, 0:1, 127:7, Len:64, (unmask(MaskKey, Rest))/bytes>>);
 decode_frame(<<Fin:1, Reserved:3, Op:4, Mask:1, 127:7, Len:64, Payload/bytes>>) ->
 	Len = byte_size(Payload), % TODO: return some error? alternatively move to the pattern above
