f1c51b600908cce66a59853bbda47337b59d335e
flan <flan@flande.re>
2018-07-26 02:29:51 +0200

    Draw >45° lines correctly

--- src/img.erl
+++ src/img.erl
@@ -20,7 +20,10 @@ set_pixels(Img, _, []) ->
 line_fun({X1, Y1}, {X2, Y2}) ->
 	A = (Y2 - Y1) / (X2 - X1),
 	B = ((Y1 + Y2) - A * (X1 + X2)) / 2,
-	fun(X) -> A*X+B end.
+	case abs(A) > 1 of
+		true  -> {y, fun(Y) -> (Y-B)/A end};
+		false -> {x, fun(X) -> A*X+B end}
+	end.
 
 linexy(Start, End) ->
 	linexy(Start, End, 1).
@@ -30,10 +33,16 @@ linexy(Start, End, Thickness) when Thickness =/= 1 ->
 linexy({X, SY}, {X, EY}, _Thickness) ->
 	{Y1, Y2} = tsort(SY, EY),
 	[{X, round(Y)} || Y <- lists:seq(Y1, Y2)];
-linexy(Start = {SX, _}, End = {EX, _}, Thickness) ->
-	F = line_fun(Start, End),
-	{X1, X2} = tsort(SX, EX),
-	[{X, round(F(X))} || X <- lists:seq(X1, X2)].
+linexy(Start = {SX, SY}, End = {EX, EY}, Thickness) ->
+	{Axis, F} = line_fun(Start, End),
+	case Axis of
+		x ->
+			{X1, X2} = tsort(SX, EX),
+			[{X, round(F(X))} || X <- lists:seq(X1, X2)];
+		y ->
+			{Y1, Y2} = tsort(SY, EY),
+			[{round(F(Y)), Y} || Y <- lists:seq(Y1, Y2)]
+	end.
 
 line(Img, Colour, Start, End) ->
 	line(Img, Colour, Start, End, 1).
