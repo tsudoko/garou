2fdca45e21550dfcf61f270de6bef71020b24553
flan <flan@flande.re>
2018-07-27 04:22:38 +0200

    Use separate processes for layer handling (drawing etc)

--- src/garou.erl
+++ src/garou.erl
@@ -1,10 +1,26 @@
 -module(garou).
 -behaviour(ws).
 -export([handshake/1, message/2, close/2]). % behaviour(ws)
--export([start/0, loop/2]).                 % public api
+-export([loop/2, img_loop/3]).              % spawned internally
+-export([start/0]).                         % public api
 
 handshake(State) ->
 	io:format("got handshake from ~p~n", [State]).
+
+% FIXME: for some reason messages aren't passed here if there's some long running
+%        blocking task (e.g. ClearUpdate from client_init/1) until there are more
+%        messages sent after it's done, but even then you have a constant "lag"
+%
+%  poor illustration of the problem
+%
+%        | -a->        | -> |
+%        | -b->        |    | (long running task)
+%        | -c->-b->    |    | (still running)
+%        |        <-a- | <- |
+%        | -c->-b->    |    | (???? the process doesn't receive b even though it's not busy)
+%        | -d->-c->-b->| -> |
+%        |        <-b- | <- |
+%        | -d->-c->    |    | (still stuck)
 
 message(State, {text, <<"new">>}) ->
 	ID = erlang:phash2(State),
@@ -46,7 +62,7 @@ message_json(S = {_, {_, Path, _}}, [{<<"PLine">>, PLine}|Rest]) ->
 	 ,<<"G">> := G
 	 ,<<"B">> := B
 	 ,<<"A">> := A} = maps:from_list(PLine),
-	?MODULE ! {modlayer, Path, Layer, {R, G, B, A}, img:line({X0, Y0}, {X1, Y1})},
+	?MODULE ! {modlayer, Path, Layer, {R, G, B, A}, fun() -> img:line({X0, Y0}, {X1, Y1}, Size) end},
 	?MODULE ! {roomsend, Path, #{<<"User">> => Name, <<"PLine">> => PLine}},
 	message_json(S, Rest);
 message_json(S, [{<<"NameChange">>, Name}|Rest]) ->
@@ -77,20 +93,40 @@ client_init(S) ->
 	ws:send(S, text, jsx:encode(#{yourname => Name})),
 	send_layers(S, Canvas, 0).
 send_layers(S, {N, W, H, [Cur|Rest]}, Num) ->
+	Cur ! {get, self()},
+	receive {Cur, W, H, Img} -> ok end,
 	ws:send(S, text, jsx:encode(#{clearUpdate =>
 		#{<<"Clear">> => true
 		 ,<<"Layer">> => Num
-		 ,<<"Png">> => base64:encode(png:encode(Cur, W, H, rgba8))
+		 ,<<"Png">> => base64:encode(png:encode(Img, W, H, rgba8))
 		 ,'X' => 0
 		 ,'Y' => 0}})),
 	send_layers(S, {N, W, H, Rest}, Num + 1);
 send_layers(_, {_, _, _, []}, _) ->
 	ok.
 
+img_loop(W, H, Data) ->
+	receive
+		{get, Pid} ->
+			Pid ! {self(), W, H, Data},
+			img_loop(W, H, Data);
+		{draw, Color, F} ->
+			img_loop(W, H, img:draw(Color, F(), W, H, Data))
+	end.
+
+% TODO: maybe look into moving socket-specific state into the socket's
+%       controlling process? would remove the need for {getclient, ...}ing
+%       on every single message we receive
+% do note we'd still need to keep some of it in the main loop since at least
+% the name (and admin status?) are persistent between connections
+
+% TODO: do something when loop/2 crashes, would be nice to preserve state
+% worst case, kill all clients
 loop(Rooms, Connections) ->
 	receive
 		{newclient, S = {_, {_, Path, _}}, ID} ->
-			{Canvas, Users} = maps:get(Path, Rooms, {{2, 1000, 1000, [<<0:(1000*1000*32)>>, <<0:(1000*1000*32)>>]}, #{}}),
+			{Num, W, H} = {2, 1000, 1000},
+			{Canvas, Users} = maps:get(Path, Rooms, {{Num, W, H, [spawn(?MODULE, img_loop, [W, H, <<0:(W*H*32)>>]) || _ <- lists:seq(1, Num)]}, #{}}),
 			Admin = Users == #{},
 			User = maps:get(ID, Users, {Admin, gen_name()}),
 			loop(Rooms#{Path => {Canvas, Users#{ID => User}}}, Connections#{S => ID});
@@ -103,12 +139,11 @@ loop(Rooms, Connections) ->
 			#{S := ID} = Connections,
 			#{Path := {Canvas, Users}} = Rooms,
 			loop(Rooms#{Path => {Canvas, Users#{ID => Client}}}, Connections);
-		{modlayer, Path, Layer, Color, Pixels} ->
-			#{Path := {{NLayer, W, H, Layers}, Clients}} = Rooms,
-			true = NLayer >= Layer,
-			{Before, [L|After]} = lists:split(Layer, Layers),
-			{img, W, H, NewL} = img:draw({img, W, H, L}, Color, Pixels),
-			loop(Rooms#{Path => {{NLayer, W, H, Before ++ [NewL|After]}, Clients}}, Connections);
+		{modlayer, Path, Layer, Color, F} ->
+			#{Path := {{_, _, _, Layers}, _}} = Rooms,
+			% lists:nth/2 can crash, we don't handle crashes at all right now
+			lists:nth(Layer + 1, Layers) ! {draw, Color, F},
+			loop(Rooms, Connections);
 		{roomsend, Path, Msg} ->
 			[ws:send(S, text, jsx:encode(Msg)) || S = {_, {_, TargetPath, _}} <- maps:keys(Connections), TargetPath == Path],
 			loop(Rooms, Connections);
